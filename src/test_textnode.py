ace9d81b41e8634899bd1a1f4b9ee42662656734
lancerharris <lancerprogram@gmail.com>
2024-08-13 11:54:54 -0700

    Add ability to split text by delimiter
    Take a list of text nodes and split them based on first level of
    a delimiter.
    Currently not allowing the function to handle nested delimiters

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,7 +8,11 @@ from textnode import (
     text_type_code,
     text_type_image,
     text_type_link,
-    text_node_to_html_node
+    text_bold_delimiter,
+    text_italic_delimiter,
+    text_code_delimiter,
+    text_node_to_html_node,
+    split_nodes_delimiter
 )
 
 
@@ -41,7 +45,7 @@ class TestTextNode(unittest.TestCase):
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
-class TestTextNodeToHTML(unittest.TestCase):
+class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", text_type_text)
         html_node = text_node_to_html_node(node)
@@ -93,6 +97,134 @@ class TestTextNodeToHTML(unittest.TestCase):
         node = TextNode("A missing text_type node", None)
         with self.assertRaises(Exception):
             text_node_to_html_node(node)
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_single_text_node(self):
+        node = TextNode("This is a text node", text_type_text)
+        nodes = [node]
+        new_nodes = split_nodes_delimiter(nodes, text_code_delimiter, text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is a text node", text_type_text)
+            ],
+            new_nodes
+        )
+
+    def test_single_code_node(self):
+        node = TextNode("This is a `code` node", text_type_text)
+        nodes = [node]
+        new_nodes = split_nodes_delimiter(nodes, text_code_delimiter, text_type_code)
+        self.assertEqual(new_nodes[0].text, "This is a ")
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, text_type_code)
+        self.assertEqual(new_nodes[2].text, " node")
+        self.assertListEqual(
+            [
+                TextNode("This is a ", text_type_text),
+                TextNode("code", text_type_code),
+                TextNode(" node", text_type_text)
+            ],
+            new_nodes
+        )
+    
+    def test_single_code_node2(self):
+        node = TextNode("This is a `code` node with `code2`", text_type_text)
+        nodes = [node]
+        new_nodes = split_nodes_delimiter(nodes, text_code_delimiter, text_type_code)
+        self.assertEqual(new_nodes[0].text, "This is a ")
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, text_type_code)
+        self.assertEqual(new_nodes[2].text, " node with ")
+        self.assertEqual(new_nodes[3].text, "code2")
+        self.assertEqual(new_nodes[3].text_type, text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is a ", text_type_text),
+                TextNode("code", text_type_code),
+                TextNode(" node with ", text_type_text),
+                TextNode("code2", text_type_code)
+            ],
+            new_nodes
+        )
+
+    def test_single_bold_node(self):
+        node = TextNode("This is a **bold** node", text_type_text)
+        nodes = [node]
+        new_nodes = split_nodes_delimiter(nodes, text_bold_delimiter, text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is a ", text_type_text),
+                TextNode("bold", text_type_bold),
+                TextNode(" node", text_type_text),
+            ],
+            new_nodes
+        )
+
+    def test_single_italic_node(self):
+        node = TextNode("This is an *italic* node", text_type_text)
+        nodes = [node]
+        new_nodes = split_nodes_delimiter(nodes, text_italic_delimiter, text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("This is an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" node", text_type_text),
+            ],
+            new_nodes
+        )
+
+    def test_double_code_node(self):
+        node1 = TextNode("This is a `code` node", text_type_text)
+        node2 = TextNode("This is a `code2` node", text_type_text)
+        nodes = [node1, node2]
+        new_nodes = split_nodes_delimiter(nodes, text_code_delimiter, text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is a ", text_type_text),
+                TextNode("code", text_type_code),
+                TextNode(" node", text_type_text),
+                TextNode("This is a ", text_type_text),
+                TextNode("code2", text_type_code),
+                TextNode(" node", text_type_text),
+            ],
+            new_nodes
+        )
+
+    def test_multi_distinct_nodes(self):
+        node1 = TextNode("This is a `code` node", text_type_text)
+        node2 = TextNode("This is a **bold** node", text_type_text)
+        node3 = TextNode("This is a basic text node", text_type_text)
+        nodes = [node1, node2, node3]
+        new_nodes = split_nodes_delimiter(nodes, text_code_delimiter, text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is a ", text_type_text),
+                TextNode("code", text_type_code),
+                TextNode(" node", text_type_text),
+                TextNode("This is a **bold** node", text_type_text),
+                TextNode("This is a basic text node", text_type_text),
+            ],
+            new_nodes
+        )
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and *italic*", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("bold", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("italic", text_type_italic),
+            ],
+            new_nodes,
+        )
+    
+    def test_missing_closing_delimiter(self):
+        node = TextNode("This is a broken `code node", text_type_text)
+        nodes = [node]
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, text_code_delimiter, text_type_code)
 
 if __name__ == "__main__":
     unittest.main()
